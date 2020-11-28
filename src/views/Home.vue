added0214864259fc234ea579fad835bf72b4254
VargaGabor <gabor.varga87@hotmail.com>
2020-11-28 13:58:53 +0100

    up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
     <h1>Home</h1>
-    <ul>
-      <!--
+
+    <table id="input_table">
+      <tr> 
+        <td>A feladat neve:</td>
+        <td><input type="text" id="name_text"></td>
+      </tr>
+      <tr>
+        <td>A feladat állása:</td>
+        <td><input type="checkbox" id="allas">Elkészült <input type="checkbox" name="" id="">Nem keszült</td>
+      </tr>
+      <tr>
+        <td>Mikor kezdte a feladatot?</td>
+        <td><input type="date" id="date"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><input type="button" @click="addrow()" value="Kész"></td>
+      </tr>
+    </table>
+<br>
+  <p v-for="task in tasks" :key="task.id"> {{task.id}} </p>
+
+<br>
+    <!--<ul>
+      
         v-for
           Bejárunk vele egy tömböt. 
         :key
@@ -10,93 +32,118 @@
         
         a kettőspont (pl: :class, :checked) arra kell hogy ehhez a <li> elemhez kapcsolom a feltételét.
           szóval ha :class belüli érték igaz akkor fog csak érvényesülni, máskülömben nem! 
-       -->
-      <li v-for="task in tasks" :key="task.id" :class="{completed: task.completed}">
-        <input @click="changeCompleted(task.id)" type="checkbox" :checked=task.completed>
-        {{task.name}} - {{task.due}}
+       
+      <li v-for="task in tasks" :key="task.id" :class="{ completed: task.completed }">
+        <input @click="changeCompleted(task.id)" type="checkbox" :checked="task.completed" />
+        {{ task.name }} - {{ task.due }}
       </li>
-    </ul>
+    </ul> -->
+
+    <table>
+      <tr v-for="task in tasks" :key="task.id" :class="{ completed: task.completed }">
+        <input @click="changeCompleted(task.id)" type="checkbox" :checked="task.completed"/>
+        <td> {{ task.name }}</td>
+        <td>{{ task.due }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+
+var elem = document.getElementById("name_text");
+//var valami = this.tasks.last("id");
+
+
+
+//var valami = tasks[""];
+//var vissza = valami[valami.lenght-1];
+
 export default {
-  name: 'home',
+  name: "home",
 
-  components: {
+  components: {},
 
-  },
-
-  data(){
-    return{
+  data() {
+    return {
       tasks: [
         {
           id: 1,
-          name: 'Első feladat',
+          name: "Első feladat",
           completed: false,
-          due: '2020-11-21'
+          //due: "2020-11-21"
         },
         {
           id: 2,
-          name: 'Második feladat',
+          name: "Második feladat",
           completed: true,
-          due: '2020-11-21'
+          //due: "2020-11-21"
         },
         {
           id: 3,
-          name: 'Harmadik feladat',
+          name: "Harmadik feladat",
           completed: false,
-          due: '2020-11-21'
+          //due: "2020-11-21"
         },
         {
           id: 4,
-          name: 'Negyedik feladat',
+          name: "Negyedik feladat",
           completed: false,
-          due: '2020-11-21'
-        },
+          //due: "2020-11-21"
+        }
       ]
-    }
+    };
   },
   methods: {
-    changeCompleted(id){
-      let task = this.tasks.find(task => task.id == id)
-      task.completed = !task.completed
+    changeCompleted(id) {
+      let task = this.tasks.find((task) => task.id == id);
+      task.completed = !task.completed;
     },
-  },
+    addrow(){
+      this.tasks.push({ id: this.tasks.last.id , name: elem , completed: false})
+      console.log(this.tasks.last)
+    }
+  }
+};
 
-}
+
+
 </script>
 
 <style scoped>
-.completed{
+.completed {
   text-decoration: line-through;
   color: dimgray;
   border-bottom: none;
 }
 
-h1{
+h1 {
   background: black;
   color: white;
   padding: 10px;
   text-align: center;
 }
 
-ul{
+ul {
   list-style-type: none;
   margin: 0;
   padding: 0;
 }
 
-li{
+li {
   margin: 0;
   padding: 10px 10px;
   position: relative;
   border-bottom: 1px black solid;
 }
 
-li:hover{
+li:hover {
   background: black;
   color: white;
   transition: 0.275s;
+}
+
+#input_table{
+  border: 1px solid black;
 }
 </style>
